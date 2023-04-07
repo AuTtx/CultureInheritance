f9114464751da3e506fbbd19d4a9c0703eaa71ca
AuTtx <1932063838@qq.com>
2023-04-07 17:25:37 +0800

    Initial Commit

---
+++ Inheritance/InheritanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InheritanceApp.swift
+//  Inheritance
+//
+//  Created by 许智尧 on 2023/4/7.
+//
+
+import SwiftUI
+
+@main
+struct InheritanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
